7437ca99ac7a4fb7eb78a7acc214bf9ff026836a
osvaldomiranda <osvaldo.omiranda@gmail.com>
2019-09-13 11:38:23 -0300

    [ADD] VU

--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-use App\Exports\ExcelExport;
-use Maatwebsite\Excel\Facades\Excel;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/vuinvitation/{secret_a}/{secret_b}','ApiUserController@validateSecret');
-
-
-Route::get('/excel', function () {
-    return Excel::download(new ExcelExport, 'products.xlsx');
-});
 
 
 Route::get('{any}', function () {
