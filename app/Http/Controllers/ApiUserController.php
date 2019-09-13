3cf9367531a9888229d9e74b55a883393ee5e932
osvaldomiranda <osvaldo.omiranda@gmail.com>
2019-09-13 16:30:32 -0300

    [ADD] VU

--- app/Http/Controllers/ApiUserController.php
+++ app/Http/Controllers/ApiUserController.php
@@ -79,7 +79,7 @@ class ApiUserController extends Controller
             Info('token');
             Info($token);    
           
-            return redirect()->to('/')->with('token',$token)->send();
+            return redirect()->route('/')->with('token',$token);
         }else{
             return abort(404,'Error: UserAccess');
         }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="token" content="{{ session('token') }}">
+        <meta name="token" content="{{ Session::get('token') }}">
         <title>Cuantificación de Emisiones</title>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
     </head>
