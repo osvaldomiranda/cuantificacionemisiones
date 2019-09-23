8131a0eb1d9ebd7923d2b3bdda2cf7d8a5659636
osvaldomiranda <osvaldo.omiranda@gmail.com>
2019-09-23 12:40:33 -0300

    [FIX] ruta api

--- routes/api.php
+++ routes/api.php
@@ -24,13 +24,19 @@ Route::post('/solicitud_industrial', 'ApiUserController@solicitudIndustrial');
 Route::post('/requisition/approve', 'RequisitionController@approve');
 Route::post('/requisition/reject', 'RequisitionController@reject');
 
+
+
+
+
+
+
 Route::middleware('client')->group(function () {
 	Route::post('/user_invitation', 'ApiUserController@setSecretInvitation');
 	
+	Route::post('/actualizarEmpresa','CompanyController@update'); 
+	Route::post('/actualizarEstablecimiento','EstablishmentController@update');
+	Route::post('/actualizarUsuario','UserController@update');
 
-	Route::post('/api/actualizarEmpresa','CompanyController@update'); 
-	Route::post('/api/actualizarEstablecimiento','EstablishmentController@update');
-	Route::post('/api/actualizarUsuario','UserController@update');
 
 });
 
