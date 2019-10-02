ac9ad54c04da632470a3a4fbf60af86c368a331a
osvaldomiranda <osvaldo.omiranda@gmail.com>
2019-10-02 14:06:34 -0300

    [ADD] VU 2.0

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::post('/recibir_solicitud_industrial', 'ApiUserController@solicitudIndustrial');
+Route::post('/solicitud_industrial', 'ApiUserController@solicitudIndustrial');
 Route::post('/requisition/approve', 'RequisitionController@approve');
 Route::post('/requisition/reject', 'RequisitionController@reject');
 
